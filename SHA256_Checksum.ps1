6c82fbea68f6fb244d6d82288d0af84e0c142ddb
mattsch93 <43786954+mattsch93@users.noreply.github.com>
2018-10-02 22:35:45 +0100

    Add files via upload

---
+++ SHA256_Checksum.ps1
@@ -0,0 +1,2 @@
+﻿# Calculate Checksum of file(s)
+$hash1 = Get-FileHash 'C:\Users\Matthew Schmolke\Desktop\1.txt' -Algorithm SHA256 | Out-File -FilePath 'C:\Users\Matthew Schmolke\Desktop\hash1.txt'
