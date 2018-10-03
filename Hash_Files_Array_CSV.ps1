d37599b4170a0f4bad4f9fcb6e0066c92275767f
mattsch93 <43786954+mattsch93@users.noreply.github.com>
2018-10-03 01:17:47 +0100

    Add files via upload

---
+++ Hash_Files_Array_CSV.ps1
@@ -0,0 +1,12 @@
+﻿$csvFilePath = 'C:\Users\Matthew Schmolke\Desktop\RAMP\hashes2.csv'
+$files = Get-ChildItem 'C:\Users\Matthew Schmolke\Desktop\RAMP' -File
+$hashes = foreach ($file in $Files){
+    Write-Output (New-Object -TypeName PSCustomObject -Property @{
+        FileName = $file.Name
+        SHA256 = Get-FileHash $file.FullName -Algorithm SHA256 | Select-Object -ExpandProperty Hash
+        })
+}
+
+$hashes | Export-Csv -NoTypeInformation -Path $csvFilePath
+
+        
